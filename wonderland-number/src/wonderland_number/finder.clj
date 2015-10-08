0a0d1cdfec09eb03d60960965ceddad174875363
Fernando Varesi <fvaresi@gmail.com>
2015-10-08 16:53:29 -0300

    Solution to wonderland number kata

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,28 @@
 (ns wonderland-number.finder)
 
+(def num-digits 6)
+(def min-range (int (Math/pow 10 (dec num-digits))))
+(def max-range (int (Math/pow 10 num-digits)))
+
+;; (defn- digits [n]
+;;   (loop [current n
+;;          res #{}]
+;;     (if (< current 10)
+;;       (conj res current)
+;;       (recur (quot current 10) (conj res (mod current 10))))))
+(def digits (comp set str))
+
+(def factors (range 1 7))
+
+(defn- product-fns []
+  (for [factor factors]
+    (partial * factor)))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (->> (range min-range max-range)
+       (map #((apply juxt (product-fns)) %))
+       (filter #(->> %
+                     (map digits)
+                     (apply =)))
+       (first)
+       (first)))
